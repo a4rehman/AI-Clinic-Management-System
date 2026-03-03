9a7ad7072e5143a1c3ce8aa636891e0ced244707
a4rehman <rao.abdulrehman534@gmail.com>
2026-03-03 23:14:09 +0500

    Added Docker support for Render

--- index.js
+++ index.js
@@ -41,7 +41,13 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ],
+        executablePath: process.env.CHROME_PATH || '/usr/bin/google-chrome-stable'
     }
 });
 
